8c16aa9a594734d52f296a152697c4fb22e26231
hispanicdevian <iantastream@gmail.com>
2023-12-17 12:48:03 -0800

    first commits

--- src/main/kotlin/MainScreen.kt
+++ src/main/kotlin/MainScreen.kt
@@ -3,7 +3,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,46 +13,62 @@ import androidx.compose.ui.unit.dp
 @Preview
 fun mainScreen() {
     val titleList = listOf("Square 0", "Square 1", " Square 2", "Square 3")
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    var boxVisibility by remember { mutableStateOf(List(4) { true }) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            Spacer(modifier = Modifier.height(15.dp))
-//////////////////////////////////////////////////////////// It will multiply content times 4
-            repeat(4) {
-                Box(
-                    modifier = Modifier
-                        .background(color = Color.Black, shape = AbsoluteRoundedCornerShape(8.dp))
-                        .padding(5.dp)
-                        .weight(1f)
-                        .aspectRatio(1.5f)
-                        .background(
-                            (Color.DarkGray),
-                            shape = AbsoluteRoundedCornerShape(5.dp)
-                        ),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        Text(
-                            text = titleList[it],
-                            color = Color.White,
-                        )
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            text = "Subtitle",
-                            color = Color.White,
-                        )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Spacer(modifier = Modifier.height(15.dp))
+
+                // Show the Box only if it's visible
+                for (index in titleList.indices) {
+                    if (boxVisibility[index]) {
+                        Box(
+                            modifier = Modifier
+                                .background(color = Color.Black, shape = AbsoluteRoundedCornerShape(8.dp))
+                                .padding(5.dp)
+                                .weight(1f)
+                                .aspectRatio(1.5f)
+                                .background(
+                                    (Color.DarkGray),
+                                    shape = AbsoluteRoundedCornerShape(5.dp)
+                                ),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Column(
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Text(
+                                    text = titleList[index],
+                                    color = Color.White,
+                                )
+                                Spacer(modifier = Modifier.height(8.dp))
+                                Text(
+                                    text = "Subtitle",
+                                    color = Color.White,
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(12.dp))
                     }
                 }
-                Spacer(modifier = Modifier.height(12.dp))
             }
+        }
+        // Call the second screen passing a function to toggle visibility
+        secondScreen { updatedVisibility ->
+            boxVisibility = updatedVisibility
         }
     }
 }
---
+++ src/main/kotlin/SettingOnOffBoxes.kt
@@ -0,0 +1,23 @@
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+
+@Composable
+fun secondScreen(onToggleMainScreen: (List<Boolean>) -> Unit) {
+    buttonToToggleVisibility {
+        onToggleMainScreen(it)
+    }
+}
+
+@Composable
+fun buttonToToggleVisibility(onToggle: (List<Boolean>) -> Unit) {
+    Button(
+        onClick = {
+            // Toggles visibility for the first box
+            val updatedVisibility = listOf(false, true, true, true) // Update this as needed
+            onToggle(updatedVisibility)
+        }
+    ) {
+        Text(text = "Toggle Box 1")
+    }
+}
