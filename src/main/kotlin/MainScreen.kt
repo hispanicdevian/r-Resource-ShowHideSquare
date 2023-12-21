a0b36d5e5f47859b8c09ec9a135c07cd3bae7503
hispanicdevian <iantastream@gmail.com>
2023-12-21 14:48:42 -0800

    123

--- src/main/kotlin/MainScreen.kt
+++ src/main/kotlin/MainScreen.kt
@@ -8,3 +8,76 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+
+@Composable
+fun mainScreen() {
+    var currentScreen by remember { mutableStateOf<Navi>(Navi.Main) }
+    val titleList = listOf("Square 0", "Square 1", "Square 2", "Square 3")
+    val visibilityList = remember { mutableStateListOf(true, true, true, true) }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            modifier = Modifier.align(Alignment.TopCenter),
+            onClick = { currentScreen = if (currentScreen == Navi.Main) Navi.Settings else Navi.Main }
+        ) {
+            Text("Switch Screen")
+        }
+
+        Column(
+            modifier = Modifier.padding(top = 35.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            when (currentScreen) {
+                is Navi.Main -> {
+                    Spacer(modifier = Modifier.height(15.dp))
+
+                    for (index in titleList.indices) {
+                        if (visibilityList[index]) {
+                            Box(
+                                modifier = Modifier
+                                    .background(color = Color.Black, shape = AbsoluteRoundedCornerShape(8.dp))
+                                    .padding(5.dp)
+                                    .weight(1f)
+                                    .aspectRatio(1.5f)
+                                    .background(
+                                        (Color.DarkGray),
+                                        shape = AbsoluteRoundedCornerShape(5.dp)
+                                    ),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    Text(
+                                        text = titleList[index],
+                                        color = Color.White,
+                                    )
+                                    Spacer(modifier = Modifier.height(8.dp))
+                                    Text(
+                                        text = "Subtitle",
+                                        color = Color.White,
+                                    )
+                                }
+                            }
+                            Spacer(modifier = Modifier.height(12.dp))
+                        }
+                    }
+                }
+
+                Navi.Settings -> {
+                    // Render the second screen content
+                    secondScreen(visibilityList) { updatedVisibility ->
+                        visibilityList.clear()
+                        visibilityList.addAll(updatedVisibility)
+                    }
+                }
+            }
+        }
+    }
+}
+
