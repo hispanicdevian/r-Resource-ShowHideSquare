a4c2ae8c99fa008293905c109e46877269c58525
hispanicdevian <iantastream@gmail.com>
2023-12-21 14:50:15 -0800

    123

--- src/main/kotlin/SecondScreen.kt
+++
@@ -1,8 +0,0 @@
-import androidx.compose.runtime.Composable
-
-@Composable
-fun secondScreen(visibilityList: List<Boolean>, onToggle: (List<Boolean>) -> Unit) {
-    magicButton(visibilityList) { updatedList ->
-        onToggle(updatedList)
-    }
-}
