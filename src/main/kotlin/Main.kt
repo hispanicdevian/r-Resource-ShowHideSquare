3013fef6577c4615c4ed185169b5b491aacb4232
hispanicdevian <iantastream@gmail.com>
2023-12-23 10:26:43 -0800

    Formatted 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,22 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import views.mainScreen
 
+//////////////////////////////////////////////////////////// Start Normal Screen Mode
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         mainScreen()
     }
 }
+
+//////////////////////////////////////////////////////////// Start Full Screen Mode
+/*
+fun main() {
+    application {
+        val windowState = rememberWindowState(size = DpSize.Unspecified)
+        Window(onCloseRequest = ::exitApplication, state = windowState) {
+            mainScreen()
+        }
+    }
+}
+ */
